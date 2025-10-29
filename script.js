4ae5b71dc924a497842975732087c3c2cb561ed2
shaik nausheen zehra <nzehra7689@gmail.com>
2025-10-29 16:09:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+async function getWeather() {
+  const city = document.getElementById('city').value;
+  const apiKey = 'YOUR_API_KEY';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  document.getElementById('result').innerHTML = `
+    <h2>${data.name}</h2>
+    <p>Temperature: ${data.main.temp}°C</p>
+    <p>Humidity: ${data.main.humidity}%</p>
+    <p>Condition: ${data.weather[0].description}</p>
+  `;
+}
